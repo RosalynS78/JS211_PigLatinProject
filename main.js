4ce0d7c952315efc0e3d98591c57078e1e027081
RosalynS78 <rrcervania78@gmail.com>
2022-01-19 23:54:09 -0600

    update

--- main.js
+++ main.js
@@ -12,10 +12,36 @@ const rl = readline.createInterface({
 
 
 const pigLatin = (word) => {
+  // dot notations
+  // should lowercase and trim word before translation
+  // split() - splits into list
+  // input - set or return the value of text in input field
+  let input = word.trim().toLowerCase().split('');
+  // array of vowels a e i o u
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+  // includes() - returns true if string contains a specified string otherwise false
+  if(vowels.includes(input[0])) {
+    // should attach "yay" if word begins with vowel
+    return input.concat('yay').join().replace(/,/g, '');
+    // join() - returns array into string does change orignal array
+    // concat() - joins arrays and return new array
+    // g - global modifier. replace all matches
+    // replace() - searches string for value and returns new string with value replaced does not change orignal string
+    //replaces ',' with spaces
+  } else {
+    // value of 0 - less than comparison of value - increment 
+    for(let i = 0; i < input.length; i++) {
+      if(vowels.indexOf(input[i]) !== -1) {
+        // !== strict inequality
+        const beforeVowel = input.splice(0, i)
+        // splice() adds &/or removes array elements: overwrites original array
+        // personal note difference in slice() returns selected elements in array as new array: does not change original array
+        return input.concat(beforeVowel).concat('ay').join().replace(/,/g, '')
+      }
+    }
+  }
+} 
 
-  // Your code here
-
-}
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
@@ -64,8 +90,21 @@ if (typeof describe === 'function') {
 // **********
 //   HINTS
 // **********
-
+// should translate a simple word
 // break your code into pieces and focus on one piece at a time...
-// 1. if word begins with a vowel send to one function: adds "yay"
+// 1. create a word var & take whitespaces out a & make lowercase
 // 2. if word begins with a consonant send to another function: splices off beginning, returns word with new ending.
 // 3. if multiple words, create array of words, loop over them, sending them to different functions and creating a new array with the new words.
+
+
+// Rules
+//  a. if it starts with consonant, move first letter to the end & add "ay"
+//  b. if it starts with a vowel, just add "yay" to the end
+//  c. if it starts with 2 consonants, move first 2 letters to the end and add "ay"
+
+// 1. if word begins with a vowel send to one function: adds "yay"
+// 2.  array for vowels: a, e, i, o, u
+// 3. check the word where the vowel starts
+// 4. move consonants(s) if needed,
+// 5. add the appropriate ending
+// 6. return the word
